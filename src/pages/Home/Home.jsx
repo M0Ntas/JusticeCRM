d4ace901993aded0742062d801476cdc5dff8509
M0Ntas <p.chudnoff@yandex.ru>
2021-11-22 21:42:06 +0300

    update modal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,78 +1,94 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './styles.scss'
 import Modal from "../../components/Modal/Modal";
 import TitleHeader from "../../components/TitleHeader/TitleHeader";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import plus from "../../images/icons/plus.svg";
 
 const Home = () => {
-  // const title = 'Sales statistics'
-  const [open, setOpen] = useState(false)
+    // const title = 'Sales statistics'
+    const [open, setOpen] = useState(false)
 
-  const inputs = [
-    {
-      type: 'text',
-      id: 0,
-      placeholder: 'Store'
-    },
-    {
-      type: 'number',
-      id: 1,
-      placeholder: 'Price'
-    },
-    {
-      type: 'text',
-      id: 2,
-      placeholder: 'Product name'
-    },
-    {
-      type: 'text',
-      id: 3,
-      placeholder: 'Product Category'
-    },
-    {
-      type: 'number',
-      id: 4,
-      placeholder: 'Quantity of goods'
-    },
-    {
-      type: 'number',
-      id: 5,
-      placeholder: 'Weight/Volume of one item'
-    },
-  ]
+    const inputs = [
+        {
+            type: 'text',
+            id: 0,
+            placeholder: 'Store'
+        },
+        {
+            type: 'number',
+            id: 1,
+            placeholder: 'Price'
+        },
+        {
+            type: 'text',
+            id: 2,
+            placeholder: 'Product name'
+        },
+        {
+            type: 'text',
+            id: 3,
+            placeholder: 'Product Category'
+        },
+        {
+            type: 'number',
+            id: 4,
+            placeholder: 'Quantity of goods'
+        },
+        {
+            type: 'number',
+            id: 5,
+            placeholder: 'Weight/Volume of one item'
+        },
+    ]
 
 
+    return (
+        <div className="container">
+            <TitleHeader
+                title={"Sales statistics"}
+                subtitle={"Welcome to CRM dashboard"}
+                onClick={() => setOpen(true)}
+            />
+            <div className="tables">
+                <div className="tables-left">
+                    <div className="one">
+                        1
+                    </div>
+                    <div className="two">
+                        2
+                    </div>
+                </div>
+                <div className="tables-right">
+                    <div className="three">
+                        3
+                    </div>
+                </div>
+            </div>
 
-  return (
-    <div className="container">
-      <TitleHeader
-        title={"Sales statistics"}
-        subtitle={"Welcome to CRM dashboard"}
-        onClick={() => setOpen(true)}
-      />
-      <div className="tables">
-        <div className="tables-left">
-          <div className="one">
-            1
-          </div>
-          <div className="two">
-            2
-          </div>
+            {open && <Modal
+                onClick={setOpen}
+                title="Creating a product">
+                {inputs.map((item) => {
+                    return (
+                        <div className="modal-input-wrap" key={item.id}>
+                            <Input
+                                placeholder={item.placeholder}
+                                type={item.type}
+                            />
+                        </div>
+                    )
+                })}
+                <div className="modal-button">
+                    <Button onClick={() => console.log('====>Add<====')}>
+                        <span>Add products <img src={plus} alt='add'/></span>
+                    </Button>
+                </div>
+            </Modal>
+            }
         </div>
-        <div className="tables-right">
-          <div className="three">
-            3
-          </div>
-        </div>
-      </div>
-
-      {open && <Modal
-        onClick={setOpen}
-        title="Creating a product"
-        inputs={inputs}
-        button="add-product"
-      />}
-    </div>
-  )
+    )
 };
 
 export default Home;
--- src/pages/MyProducts/MyProducts.jsx
+++ src/pages/MyProducts/MyProducts.jsx
@@ -1,41 +1,57 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './styles.scss'
 import TitleHeader from "../../components/TitleHeader/TitleHeader";
 import Modal from "../../components/Modal/Modal";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 const MyProducts = () => {
-  const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
 
-  const inputs = [
-    {
-      type: 'number',
-      id: 0,
-      placeholder: 'Number of products'
-    },
-    {
-      type: 'date',
-      id: 1,
-      placeholder: 'Date of sale'
-    },
-  ]
+    const inputs = [
+        {
+            type: 'number',
+            id: 0,
+            placeholder: 'Number of products'
+        },
+        {
+            type: 'date',
+            id: 1,
+            placeholder: 'Date of sale'
+        },
+    ]
 
-  return (
-    <div className="container">
-      <TitleHeader
-        title={"My product"}
-        subtitle={"Product table"}
-        onClick={() => setOpen(true)}
-      />
+    return (
+        <div className="container">
+            <TitleHeader
+                title={"My product"}
+                subtitle={"Product table"}
+                onClick={() => setOpen(true)}
+            />
 
-      {open && <Modal
-        onClick={setOpen}
-        title={"Sell the product"}
-        inputs={inputs}
-        button="sell-product"
-      />}
-    </div>
+            {open && <Modal
+                onClick={setOpen}
+                title="sdfsdfsfdf">
+                {inputs.map((item) => {
+                    return (
+                        <div className="modal-input-wrap" key={item.id}>
+                            <Input
+                                placeholder={item.placeholder}
+                                type={item.type}
+                            />
+                        </div>
+                    )
+                })}
+                <div className="modal-button">
+                    <Button onClick={() => console.log('====>Add<====')}>
+                        <span>xyi</span>
+                    </Button>
+                </div>
+            </Modal>
+            }
+        </div>
 
-  )
+    )
 };
 
 export default MyProducts;
