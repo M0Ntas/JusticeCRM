0891ff9ba4f6e1bef881410a50d11408d817f1ed
M0Ntas <p.chudnoff@yandex.ru>
2021-11-24 15:07:23 +0300

    revision table

--- src/components/MainTable/MainTable.jsx
+++ src/components/MainTable/MainTable.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
@@ -12,16 +12,42 @@ import './style.scss'
 import Modal from "../Modal/Modal";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
+import { styled } from '@mui/material/styles';
 
 const MainTable = ({headTable, items, setItems, onClick}) => {
 
-  const [sell, setSell] = useState(false)
-  const [redact, setRedact] = useState(false)
+  const StyledTableCell = styled(TableCell)(({theme}) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#2B3844",
+      color: "white",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({theme}) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      boxShadow: "none",
+      borderRadius: "3",
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+  const [sell, setSell] = useState(false);
+
+  const [redact, setRedact] = useState(false);
 
   const [form, setForm] = useState({
     numberOfProducts: '',
     dateOfSale: '',
-  })
+  });
 
   const sellInputs = [
     {
@@ -36,7 +62,7 @@ const MainTable = ({headTable, items, setItems, onClick}) => {
       placeholder: 'Date of sale',
       handler: 'dateOfSale'
     },
-  ]
+  ];
 
   const editInputs = [
     {
@@ -84,112 +110,107 @@ const MainTable = ({headTable, items, setItems, onClick}) => {
       [key]: event.target.value
     })
     console.log('====>form<====', form)
-  }
+  };
 
   const deleteProduct = (id) => {
     setItems(items.filter(el => el.id !== id))
-  }
+  };
+
 
   return (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{minWidth: 650}} aria-label="simple table">
+    <TableContainer component={Paper}>
+      <Table sx={{minWidth: 700}} aria-label="customized table">
         <TableHead>
           <TableRow>
             {headTable.map(item => {
               return (
-                <TableCell className="table-header-row" align="center">{item}</TableCell>
+                <StyledTableCell className="table-header-row" align="center">{item}</StyledTableCell>
               )
             })}
           </TableRow>
         </TableHead>
-
         <TableBody>
-          {/*<TableRow>*/}
+          {items.map((item) => (
+            <StyledTableRow key={items.productName}>
+              <StyledTableCell component="th" scope="row">
+                {item.productName}
+              </StyledTableCell>
+              <StyledTableCell align="center">{item.store}</StyledTableCell>
+              <StyledTableCell align="center">{item.address}</StyledTableCell>
+              <StyledTableCell align="center">{item.category}</StyledTableCell>
+              <StyledTableCell align="center">{item.creationDate}</StyledTableCell>
+              <StyledTableCell align="center">{item.price}</StyledTableCell>
+              <StyledTableCell align="center">{item.remains}</StyledTableCell>
+              <StyledTableCell align="center">{item.volume}</StyledTableCell>
+              <StyledTableCell align="center">
+                {item.salesDate
+                  ? item.salesDate
+                  : (
+                    <div className="buttons">
+                      <button className='table-button'
+                              onClick={() => setSell(true)}
+                      >Sell
+                      </button>
+                      <button className='table-button'
+                              onClick={() => setRedact(true)}><img src={edit} alt='edit'/></button>
+                      <button className='table-button-delete' onClick={() => deleteProduct(item.id)}><img src={del}
+                                                                                                          alt='close'/>
+                      </button>
 
-          {items.map(item => {
-            return (
-              <TableRow className="table-row">
-                <TableCell component="th" scope="row">
-                  {item.productName}
-                </TableCell>
-                <TableCell align="center">{item.store}</TableCell>
-                <TableCell align="center">{item.address}</TableCell>
-                <TableCell align="center">{item.category}</TableCell>
-                <TableCell align="center">{item.creationDate}</TableCell>
-                <TableCell align="center">{item.price}</TableCell>
-                <TableCell align="center">{item.remains}</TableCell>
-                <TableCell align="center">{item.volume}</TableCell>
-                <TableCell align="right">
-                  {item.salesDate
-                    ? item.salesDate
-                    : (
-                      <div className="buttons">
-                        <button className='table-button'
-                                onClick={() => setSell(true)}
-                        >Sell
-                        </button>
-                        <button className='table-button'
-                                onClick={() => setRedact(true)}><img src={edit} alt='edit'/></button>
-                        <button className='table-button-delete' onClick={() => deleteProduct(item.id)}><img src={del}
-                                                                                                            alt='close'/>
-                        </button>
+                      {sell && <Modal
+                        onClick={setSell}
+                        title="Sell the product">
+                        {sellInputs.map((item) => {
+                          return (
+                            <div className="modal-input-wrap" key={item.id}>
+                              <Input
+                                placeholder={item.placeholder}
+                                type={item.type}
+                                handler={item.handler}
+                                onChange={changeHandler}
+                              />
+                            </div>
+                          )
+                        })}
+                        <div className="modal-button">
+                          <Button onClick={() => console.log('====>SellProduct<====')}>
+                            <span>Sell product</span>
+                          </Button>
+                        </div>
+                      </Modal>
+                      }
 
-                        {sell && <Modal
-                          onClick={setSell}
-                          title="Sell the product">
-                          {sellInputs.map((item) => {
-                            return (
-                              <div className="modal-input-wrap" key={item.id}>
-                                <Input
-                                  placeholder={item.placeholder}
-                                  type={item.type}
-                                  handler={item.handler}
-                                  onChange={changeHandler}
-                                />
-                              </div>
-                            )
-                          })}
-                          <div className="modal-button">
-                            <Button onClick={() => console.log('====>SellProduct<====')}>
-                              <span>Sell product</span>
-                            </Button>
-                          </div>
-                        </Modal>
-                        }
-
-                        {redact && <Modal
-                          onClick={setRedact}
-                          title="Sell the product">
-                          {editInputs.map((item) => {
-                            return (
-                              <div className="modal-input-wrap" key={item.id}>
-                                <Input
-                                  placeholder={item.placeholder}
-                                  type={item.type}
-                                  handler={item.handler}
-                                  onChange={changeHandler}
-                                />
-                              </div>
-                            )
-                          })}
-                          <div className="modal-button">
-                            <Button onClick={() => console.log('====>SellProduct<====')}>
-                              <span>Save changes</span>
-                            </Button>
-                          </div>
-                        </Modal>
-                        }
-                      </div>
-                    )
-                  }
-                </TableCell>
-              </TableRow>
-            )
-          })}
+                      {redact && <Modal
+                        onClick={setRedact}
+                        title="Sell the product">
+                        {editInputs.map((item) => {
+                          return (
+                            <div className="modal-input-wrap" key={item.id}>
+                              <Input
+                                placeholder={item.placeholder}
+                                type={item.type}
+                                handler={item.handler}
+                                onChange={changeHandler}
+                              />
+                            </div>
+                          )
+                        })}
+                        <div className="modal-button">
+                          <Button onClick={() => console.log('====>SellProduct<====')}>
+                            <span>Save changes</span>
+                          </Button>
+                        </div>
+                      </Modal>
+                      }
+                    </div>
+                  )
+                }
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
-};
-
+}
 export default MainTable;
