397162f27b5624088fdf2de4dbaead40e8bbbb3c
M0Ntas <p.chudnoff@yandex.ru>
2021-12-02 12:30:42 +0300

    add utils errorHandler

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
 
 module.exports.login = async (req, res) => {
   const candidate = await User.findOne({email: req.body.email})
@@ -53,6 +54,7 @@ module.exports.register = async (req, res) => {
       res.status(201).json(user)
     } catch (e) {
       // Обработать ошибку
+      errorHandler(res, e)
     }
   }
 }
