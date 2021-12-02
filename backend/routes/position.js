c6ef297c020cd17de0426410b0bdb70061e7bfe8
M0Ntas <p.chudnoff@yandex.ru>
2021-12-02 14:26:12 +0300

    fix bag

--- backend/routes/position.js
+++ backend/routes/position.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get('/:categoryId', controller.getByCategoryId)
 router.post('/', controller.create)
-// router.patch('/:id', controller.update)
+router.patch('/:id', controller.update)
 router.delete('/:id', controller.remove)
 
 module.exports = router
