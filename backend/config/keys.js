24345b3fa2f2a37a2ab1e156b22a5f7869af13e9
M0Ntas <p.chudnoff@yandex.ru>
2021-12-02 11:28:22 +0300

    add login, create token

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports ={
-  mongoURI: 'mongodb+srv://men_tos:80951480588@crmbase.hiwpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://men_tos:80951480588@crmbase.hiwpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
 }
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,24 +1,46 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
-module.exports.login = (req, res) => {
+module.exports.login = async (req, res) => {
+  const candidate = await User.findOne({email: req.body.email})
 
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password,
+  if (candidate) {
+    //пользователь существует
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult) {
+      // мы должны сгенерировать токен, пароли совпали
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id,
+      }, keys.jwt, {expiresIn: 60 * 60})
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      res.status(401).json({
+        //Пароли не совпали
+        massage: 'Password mismatch. Try again.'
+      })
     }
-  })
+  } else {
+    //пользователя нет
+    res.status(404).json({
+      massage: 'User with this email was not found.'
+    })
+  }
 }
 
 module.exports.register = async (req, res) => {
   const candidate = await User.findOne({email: req.body.email})
-  if (candidate){
+  if (candidate) {
     //пользователь существует, нужно дать ошибку
     res.status(409).json({
       massage: 'This email is already taken. Try another.'
     })
-  }else {
+  } else {
     //создать пользователя
     const salt = bcrypt.genSaltSync(10)
     const password = req.body.password
